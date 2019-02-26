90dfdcefc2b741baac689d8cc9bb9ba001b06319
colelarsen <31255098+colelarsen@users.noreply.github.com>
2019-02-26 07:37:28 -0500

    Update YugiohCSVScraper.js
    Added more comments where needed

--- YugiohCSVScraper.js
+++ YugiohCSVScraper.js
@@ -1,8 +1,8 @@
 var standard_input = process.stdin;
-
 // Set input character encoding.
 standard_input.setEncoding('utf-8');
 
+//-------------Requirements----------------
 //JavaScript file used for actual scraping
 const scraper = require('./scraper.js');
 //Config file with variables user should change
@@ -10,14 +10,16 @@ const config = require('./config.js');
 const fs = require('fs');
 var parse = require('csv-parse');
 var LocalStorage = require('node-localstorage').LocalStorage,
+    
 localStorage = new LocalStorage('./scratch');
-
-
 //Variables the user should not be able to change
 var urlstart = 'https://db.ygoprodeck.com/card/?search=';
 
 //Program Start
 startProgram();
+
+//Loads csvLocation and folderLocation from config file or localStorage
+//Transitions to prompt controller topLevelInput
 function startProgram()
 {
     //Load
@@ -42,39 +44,49 @@ function startProgram()
 }
 
 //Handles User Input
+//Most imputs will call topLevelInput() after they run as to wait for more user input
 function topLevelInput()
 {
     //Terminal Prompt
     prompt(':', function (input) {
-        //topLevelInput function of program
+        
+        //download prompts for the csv file name and scans the csv to make sure it exists
+        //readCSV throws and error if it cannot find the csv file
         if(input == "download")
         {
             //Waits for csv file name
             prompt('Input csv name:', function (csvName) {
                 try
                 {
+                    //If csvName ends with '.csv' remove '.csv'
                     if(csvName.endsWith(".csv"))
                     {
                         csvName = csvName.substring(0, csvName.length-3);
                     }
-                    //Reads csv data and sends it to download
+                    //Reads csv data and sends it to downloadCards
                     readCSV(config.csvLocation, csvName);
                 }
+                //There was an error locating the csv file
                 catch(err)
                 {
                     console.log("Invalid csv location: " + config.csvLocation);
+                    console.log("or\nInvalid csv name: " + csvName);
                     topLevelInput();
                 }
             });
         }
-        //Download cards by name
+        
+        //Download cards by typing into the terminal
+        //resets cardNames and calls typeCardNames
         else if(input == "download type")
         {
             cardNames = [];
             console.log("Enter card names one at a time, when finished type 'done'");
             typeCardNames();
         }
-        //Sets folder location
+        
+        //Sets folder location in localStorage and config
+        //if folderLoc does not end in '\\' add '\\' to it
         else if(input == "set folder")
         {
             prompt('Input folder location (the location of the folder, not inside the folder):', function (folderLoc) {
@@ -87,7 +99,9 @@ function topLevelInput()
                 topLevelInput();
             });
         }
-        //Sets csv location
+        
+        //Sets csv location in localStorage and config
+        //if csvLoc does not end in '\\' add '\\' to it
         else if(input == "set csv")
         {
             prompt('Input location of csv file:', function (csvLoc) {
@@ -100,7 +114,8 @@ function topLevelInput()
                 topLevelInput();
             });
         }
-        //displays all commands
+        
+        //displays all commands and extra advice
         else if(input == "help" || input == 'h')
         {
             console.log("Enter 'download' to start download proccess\n" +
@@ -113,12 +128,12 @@ function topLevelInput()
             
             "- DO NOT type double '\\' when  seting folder locations from the program\n" +
             "- Make sure to end all filepaths with a '\\' Example: 'C:\\Users\\colea\\Downloads\\Yugioh\\CSV\\'\n" +
-            "- The program will ask you for the csv file name, provide just the file name without the .csv\n" +
             "- The program will ask you for the folder name. It will look in the provided folder location for a folder with the name you enter, it will save the jpgs to the folder name you provide.\n" +
             "- If a card downloads with a weird image it means the name was spelled wrong.");
             console.log("\n\nCSV FORMAT: Obtain a .csv file. This is easily done by using Google Sheets and exporting to a .csv file. Make sure to type card names in different boxes horizontally. If you want multiple copies of a card end the card name with ` x2` or ` x3` MAKE SURE TO INCLUDE A SPACE BEFORE THE `xn`.")
             topLevelInput();
         }
+        
         //displays csv location and folder location
         else if(input == "display")
         {
@@ -126,11 +141,13 @@ function topLevelInput()
             console.log("folder location: " + "\'" + config.folderLocation + "\'\n");
             topLevelInput();
         }
+        
         //Exits program
         else if(input == 'q' || input == "quit" || input == "exit")
         {
             process.exit();
         }
+        
         //If invalid command wait for new prompt
         else
         {
@@ -139,10 +156,15 @@ function topLevelInput()
     });
 }
 
+//typeCardNames handles user input with relation to card names
+//typeCardName defaults user input to being a card name.
+//card names are tested for being valid before being added to the cardNames array
 var cardNames = [];
 function typeCardNames()
 {
     prompt(': ', function (input) {
+        
+        //If user is finished entering card names move on to downloadCards unless the cardNames list is empty
         if(input == "done" || input == "finished")
         {
             if(cardNames.length == 0)
@@ -157,6 +179,8 @@ function typeCardNames()
                 });
             }
         }
+        
+        //Displays all functions available to the user
         else if(input == "help" || input == "h")
         {
             console.log("Enter 'display' to display current cards");
@@ -167,6 +191,8 @@ function typeCardNames()
             console.log("End your card names with ' x2' or ' x3' to have multiple copies downloaded");
             typeCardNames();
         }
+        
+        //Removes a card at 'x' and displays the current list
         else if(input.startsWith("remove "))
         {
             var removeNum = input.split(" ")[1];
@@ -174,37 +200,48 @@ function typeCardNames()
             displayCardNames();
             typeCardNames();
         }
+        
+        //displays the current list of cards
         else if(input == "display")
         {
             console.log(cardNames);
             displayCardNames();
             typeCardNames();
         }
+        
+        //traverses back to topLevelInput
         else if(input == "cancel" || input == "stop" || input == "quit" || input == "q")
         {
             console.log("Canceled 'download type'")
             topLevelInput();
         }
 
-        //Input must be a card name, check to make sure card name is valid
+        //Input defaults to a card name, check to make sure card name is valid
         else
         {
+            //Call the cardTest function
             var cardTest = urlstart + scraper.convertCardToUrl(input);
             scraper.cardTest(cardTest, (data) => {
+                //If the img returned is this exact link it means the card was not found in the database
                 if(data.img == "https://ygoprodeck.com/wp-content/uploads/2018/01/card_db_twitter_Card2.jpg")
                 {
+                    //Card does not exist so do not add it to the list of cards
                     console.log(input + "    Is not a valid card name");
                 }
                 else
                 {
+                    //Add the card to cardNames
                     cardNames.push(input);
                 }
+                //call typeCardNames inside the return function so the user does not make more inputs before the program can check
+                //if the last card entered was valid
                 typeCardNames();
             });
         }
     });
 }
 
+//Display the names of cards in cardNames
 function displayCardNames()
 {
     var i = 0;
@@ -224,11 +261,14 @@ function displayCardNames()
 //Sends 'csvData', 'folderName' to downloadCards
 function readCSV(inputPath, filename)
 {
+    //read csv fileData
     fs.readFile(inputPath+filename+'.csv', function (err, fileData) {
         try
         {
+            //parse fileData into a series of rows
             parse(fileData, {columns: false, trim: true}, function(err, rows) {
-                //Combine Rows
+                
+                //Combine Rows into one array csvData
                 var i;
                 var csvData = [];
                 for(i = 0; i < rows.length; i++)
@@ -242,6 +282,8 @@ function readCSV(inputPath, filename)
                         }
                     }
                 }
+                
+                //Try to download the cards
                 try
                 {
                     prompt('Input folder name:', function (folderName) {
@@ -250,7 +292,8 @@ function readCSV(inputPath, filename)
                 }
                 catch(err)
                 {
-                    console.log("Invalid folder location: \'" + config.folderLocation + "\'");
+                    //Folder location is most likely cause of any errors in downloadCards
+                    console.log("Invalid folder location: '" + config.folderLocation + "'");
                     topLevelInput();
                 }
             });  
